979743e6a266a8eddc1d9f6ebc5b79c8a52df8b3
-akku- <151517253+akku1139@users.noreply.github.com>
2024-06-28 14:11:45 +0900

    ダサすぎるtypoを修正

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 import LinkButton from "~/components/linkButton";
 
 export default () => <>
-  <div class="text-3xl">暗記アプリへようそこ</div>
+  <div class="text-3xl">暗記アプリへようこそ</div>
   <LinkButton href="/quiz">始める</LinkButton>
   <LinkButton href="/sets">セット一覧</LinkButton>
 </>
